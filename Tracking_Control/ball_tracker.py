1148ffd63e3fcc7b1a3a7e5b319814fec32f6ad5
jamescheng40 <cheng-james@hotmail.com>
2021-02-18 18:10:29 -0500

    debugging stfu

--- Tracking_Control/ball_tracker.py
+++ Tracking_Control/ball_tracker.py
@@ -106,84 +106,84 @@ if use_TPU:
 
 
 
-picar.setup()
-# Show image captured by camera, True to turn on, you will need #DISPLAY and it also slows the speed of tracking
-show_image_enable   = True
-draw_circle_enable  = True
-scan_enable         = False
-rear_wheels_enable  = False
-front_wheels_enable = True
-pan_tilt_enable     = True
+# picar.setup()
+# # Show image captured by camera, True to turn on, you will need #DISPLAY and it also slows the speed of tracking
+# show_image_enable   = True
+# draw_circle_enable  = True
+# scan_enable         = False
+# rear_wheels_enable  = False
+# front_wheels_enable = True
+# pan_tilt_enable     = True
 
-# if (show_image_enable or draw_circle_enable) and "DISPLAY" not in os.environ:
-#     print('Warning: Display not found, turn off "show_image_enable" and "draw_circle_enable"')
-#     show_image_enable   = False
-#     draw_circle_enable  = False
+# # if (show_image_enable or draw_circle_enable) and "DISPLAY" not in os.environ:
+# #     print('Warning: Display not found, turn off "show_image_enable" and "draw_circle_enable"')
+# #     show_image_enable   = False
+# #     draw_circle_enable  = False
 
-# kernel = np.ones((5,5),np.uint8)
-# img = cv2.VideoCapture(-1)
+# # kernel = np.ones((5,5),np.uint8)
+# # img = cv2.VideoCapture(-1)
 
-SCREEN_WIDTH = 352
-SCREEN_HIGHT = 288
-# img.set(3,SCREEN_WIDTH)
-# img.set(4,SCREEN_HIGHT)
-CENTER_X = SCREEN_WIDTH/2
-CENTER_Y = SCREEN_HIGHT/2
-BALL_SIZE_MIN = SCREEN_HIGHT/5
-BALL_SIZE_MAX = SCREEN_HIGHT/3
+# SCREEN_WIDTH = 352
+# SCREEN_HIGHT = 288
+# # img.set(3,SCREEN_WIDTH)
+# # img.set(4,SCREEN_HIGHT)
+# CENTER_X = SCREEN_WIDTH/2
+# CENTER_Y = SCREEN_HIGHT/2
+# BALL_SIZE_MIN = SCREEN_HIGHT/5
+# BALL_SIZE_MAX = SCREEN_HIGHT/3
 
-#PID Control for Servo Control
-Constant_P = 2
-Constant_I = 0.1
-Constant_D = 0.08
+# #PID Control for Servo Control
+# Constant_P = 2
+# Constant_I = 0.1
+# Constant_D = 0.08
 
-#Intercommunicate Variables
-# Ymin, Ymax, Xmin, Xmax
+# #Intercommunicate Variables
+# # Ymin, Ymax, Xmin, Xmax
 
-# Filter setting, DONOT CHANGE
-hmn = 12
-hmx = 37
-smn = 96
-smx = 255
-vmn = 186
-vmx = 255
+# # Filter setting, DONOT CHANGE
+# hmn = 12
+# hmx = 37
+# smn = 96
+# smx = 255
+# vmn = 186
+# vmx = 255
 
-# camera follow mode:
-# 0 = step by step(slow, stable), 
-# 1 = calculate the step(fast, unstable)
-follow_mode = 1
+# # camera follow mode:
+# # 0 = step by step(slow, stable), 
+# # 1 = calculate the step(fast, unstable)
+# follow_mode = 1
 
-CAMERA_STEP = 2
-CAMERA_X_ANGLE = 20
-CAMERA_Y_ANGLE = 20
+# CAMERA_STEP = 2
+# CAMERA_X_ANGLE = 20
+# CAMERA_Y_ANGLE = 20
 
-MIDDLE_TOLERANT = 5
-PAN_ANGLE_MAX   = 170
-PAN_ANGLE_MIN   = 10
-TILT_ANGLE_MAX  = 150
-TILT_ANGLE_MIN  = 70
-FW_ANGLE_MAX    = 90+50
-FW_ANGLE_MIN    = 90-50
+# MIDDLE_TOLERANT = 5
+# PAN_ANGLE_MAX   = 170
+# PAN_ANGLE_MIN   = 10
+# TILT_ANGLE_MAX  = 150
+# TILT_ANGLE_MIN  = 70
+# FW_ANGLE_MAX    = 90+50
+# FW_ANGLE_MIN    = 90-50
 
-SCAN_POS = [[20, TILT_ANGLE_MIN], [50, TILT_ANGLE_MIN], [90, TILT_ANGLE_MIN], [130, TILT_ANGLE_MIN], [160, TILT_ANGLE_MIN], 
-            [160, 80], [130, 80], [90, 80], [50, 80], [20, 80]]
+# SCAN_POS = [[20, TILT_ANGLE_MIN], [50, TILT_ANGLE_MIN], [90, TILT_ANGLE_MIN], [130, TILT_ANGLE_MIN], [160, TILT_ANGLE_MIN], 
+#             [160, 80], [130, 80], [90, 80], [50, 80], [20, 80]]
 
-bw = back_wheels.Back_Wheels()
-fw = front_wheels.Front_Wheels()
-pan_servo = Servo.Servo(1)
-tilt_servo = Servo.Servo(2)
-picar.setup()
+# bw = back_wheels.Back_Wheels()
+# fw = front_wheels.Front_Wheels()
+# pan_servo = Servo.Servo(1)
+# tilt_servo = Servo.Servo(2)
+# picar.setup()
 
-fw.offset = 0
-pan_servo.offset = 0
-tilt_servo.offset = 0
+# fw.offset = 0
+# pan_servo.offset = 0
+# tilt_servo.offset = 0
 
-bw.speed = 0
-fw.turn(90)
-pan_servo.write(90)
-tilt_servo.write(90)
+# bw.speed = 0
+# fw.turn(90)
+# pan_servo.write(90)
+# tilt_servo.write(90)
 
-motor_speed = 40
+# motor_speed = 40
 
 def nothing(x):
     pass
