acf4182dcaf39135963291b4a0f90a8a8d4bb265
jamescheng40 <cheng-james@hotmail.com>
2021-02-18 19:32:45 -0500

    debugging stfu

--- Tracking_Control/ball_tracker.py
+++ Tracking_Control/ball_tracker.py
@@ -1,10 +1,9 @@
-from picar import front_wheels, back_wheels
-from picar.SunFounder_PCA9685 import Servo
-import picar
-from time import sleep
+# from picar import front_wheels, back_wheels
+# from picar.SunFounder_PCA9685 import Servo
+# import picar
+#from time import sleep
 import cv2
 import numpy as np
-import picar
 import os
 from threading import Thread
 import importlib.util
