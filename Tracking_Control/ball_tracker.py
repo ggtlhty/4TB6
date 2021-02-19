d4e15b90c88f76b9ef13c465a3545defb367e56e
jamescheng40 <cheng-james@hotmail.com>
2021-02-18 19:34:10 -0500

    added webcam

--- Tracking_Control/ball_tracker.py
+++ Tracking_Control/ball_tracker.py
@@ -1,7 +1,7 @@
-# from picar import front_wheels, back_wheels
-# from picar.SunFounder_PCA9685 import Servo
-# import picar
-#from time import sleep
+from picar import front_wheels, back_wheels
+from picar.SunFounder_PCA9685 import Servo
+import picar
+from time import sleep
 import cv2
 import numpy as np
 import os
