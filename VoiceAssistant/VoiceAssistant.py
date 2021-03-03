d298d0602ae102325f0980fd24297af4e478f0e0
JL <39628854+JohnnyLee6@users.noreply.github.com>
2021-03-02 20:17:41 -0500

    Add files via upload

--- VoiceAssistant/VoiceAssistant.py
+++ VoiceAssistant/VoiceAssistant.py
@@ -2,9 +2,15 @@
 import mysql.connector
 import pandas as pd
 import speech_recognition as sr
+import subprocess
 import os
 
-db = mysql.connector.connect(host="", user="", password="", database="grocerymatedb")
+def execute_unix(inputcommand):
+   p = subprocess.Popen(inputcommand, stdout=subprocess.PIPE, shell=True)
+   (output, err) = p.communicate()
+   return output
+
+db = mysql.connector.connect(host="grocerymate.ciihxnyiyqwe.ca-central-1.rds.amazonaws.com", user="admin", password="grocerymate", database="grocerymatedb")
 cursor = db.cursor()
 cursor.execute("SELECT * FROM shop_1")
 result = cursor.fetchall()
@@ -23,6 +29,7 @@ products = [product[0] for product in product_result]
 
 
 r = sr.Recognizer()
+assistantOn = False
 
 while True:
     with sr.Microphone() as source:
@@ -30,29 +37,39 @@ while True:
         print("audio heard")
 
     try:
-        '''
-        for testing purposes, we're just using the default API key
-        to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        instead of `r.recognize_google(audio)`
-        '''
-        
         detected_audio = r.recognize_google(audio).lower()
-        # detected_audio = "coca-cola"
-        print("Google Speech Recognition thinks you said: " + '"' + detected_audio + '"')
-        if detected_audio in categories:
+        if detected_audio == "hamate" or detected_audio == "hey mate":
+            assistantOn = True
+            print("Google Speech Recognition thinks you said: " + "hey mate")
+            print("entered into assistant mode")
+            text = "hi, what are you looking for?"
+        elif detected_audio in categories:
+            print("Google Speech Recognition thinks you said: " + '"' + detected_audio + '"')
             print(df.loc[df['category']==detected_audio])
-            print(detected_audio, "could be found in aisle: ", df.loc[df['category']==detected_audio]['aisle'].tolist()[0])
-            
+            text = detected_audio + " could be found in aisle " + str(df.loc[df['category']==detected_audio]['aisle'].tolist()[0]) + "."
+            text += " Anything else?"
+            print(text)
         elif detected_audio in products:
+            print("Google Speech Recognition thinks you said: " + '"' + detected_audio + '"')
             print(df.loc[df['product']==detected_audio])
-            print(detected_audio, "could be found in aisle: ", df.loc[df['product']==detected_audio]['aisle'].tolist()[0])
-        elif detected_audio == "stop":
-            break
+            text = detected_audio + " could be found in aisle " + str(df.loc[df['product']==detected_audio]['aisle'].tolist()[0]) + "."
+            text += " Anything else?"
+            print(text)
+        elif detected_audio == "stop" or detected_audio == "thank you":
+            print("Google Speech Recognition thinks you said: " + '"' + detected_audio + '"')
+            text = "You are welcome."
+            #break
         else:
-            print("sorry, could not find " + detected_audio + " for you")
+            text = "sorry, could not find " + detected_audio + " for you. A staff will be with you shortly."
+            print(text)
+        speech = 'espeak -ven-us "%s" 2>>/dev/null' % text
+        execute_unix(speech)
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        text = "Google Speech Recognition could not understand audio"
+        print(text)
+        #speech = 'espeak -ven-us "%s" 2>>/dev/null' % text
+        #execute_unix(speech)
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
+        text = "Could not request results from Google Speech Recognition service; {0}".format(e)
+        #speech = 'espeak -ven-us "%s" 2>>/dev/null' % text
+        #execute_unix(speech)
