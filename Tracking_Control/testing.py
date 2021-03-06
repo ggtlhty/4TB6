29187521dbf7dbcbaebfe2d45648d8a98d4d12ee
Jamescheng40 <35903019+Jamescheng40@users.noreply.github.com>
2021-03-06 18:13:15 -0500

    Update testing.py

--- Tracking_Control/testing.py
+++ Tracking_Control/testing.py
@@ -95,6 +95,7 @@ print("Begin!")
 #for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 while True:
    fw_angle = 96
+   pan_servo.write(90)
    fw.turn(fw_angle)
    bw.speed = 30
    bw.backward()
