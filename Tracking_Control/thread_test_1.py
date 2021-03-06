d119ef6e58d990e9474d5bdde4eb850b46f6946d
Jamescheng40 <35903019+Jamescheng40@users.noreply.github.com>
2021-03-06 18:11:21 -0500

    Update thread_test_1.py

--- Tracking_Control/thread_test_1.py
+++ Tracking_Control/thread_test_1.py
@@ -359,7 +359,7 @@ def control_module_thread():
                     fw.turn(fw_angle)
     #          if rear_wheels_enable:
                     pid_speed = int(1.5 * (1200-r)/1200*60)
-	            if pid_speed > 60 :
+                    if pid_speed > 60:
                         pid_speed = 60
                     bw.speed = pid_speed
                     bw.backward()
@@ -369,7 +369,7 @@ def control_module_thread():
     #           bw.backward()
             else:
                 pid_speed = int(1.5 * (r-1200)/1200*60)
-                if pid_speed > 60 :
+                if pid_speed > 60:
                     pid_speed = 60
                 bw.speed = pid_speed
                 bw.forward()
